17fab5ee1f651e8c37a7b6b07ba6cbfff21e4ec3
Pabcargam <pablo.cardenal117@gmail.com>
2021-10-09 18:14:56 +0000

    Arreglo de errores: 1. Nombre del modulo "smtp" de Django 2. Error en el modelo de cuenta de usuario

--- backend/user_accounts/models.py
+++ backend/user_accounts/models.py
@@ -17,9 +17,11 @@ class UserAccountManager(BaseUserManager):
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    name =  models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
@@ -29,6 +31,6 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def get_short_name(self):
         return self.name
-
+    
     def __str__(self):
         return self.email
