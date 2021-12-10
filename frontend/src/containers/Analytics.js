119644988e1d7f1597524f4e4464d44f3f03dcb6
Pabcargam <pablo.cardenal117@gmail.com>
2021-12-10 22:18:42 +0000

    Cambiar datos actuales por datos del dia anterior.

--- frontend/src/containers/Analytics.js
+++ frontend/src/containers/Analytics.js
@@ -13,16 +13,18 @@ const Analytics = () => {
     const [pump, setPump] = useState(false);
 
 
-        // --- ENERGY SAVE DATA STATES --- //
+        // --- ENERGY SAVE & PERFORMANCE DATA STATES --- //
 
-        const [currentTankTempDiff, setCurrentTankTempDiff] = useState(0);
-        const [lightPriceGenTime, setLightPriceGenTime] = useState(0);
-        const [performanceTempDiff, setPerformanceTempDiff] = useState(0);
+        const [maxTankTemp, setmaxTankTemp] = useState(0);
+        const [maxTime, setMaxTime] = useState('');
+        const [minTankTemp, setMinTankTemp] = useState(0);
+        const [minTime, setMinTime] = useState('');
+        const [lightPriceYesterdayGenTime, setLightPriceYesterdayGenTime] = useState(0);
 
 
     // --- ELECTRICITY DATA STATES --- //
 
-    const[lightPrice, setLightPrice] = useState(0);
+    const[currentLightPrice, setCurrentLightPrice] = useState(0);
 
 
     /*
@@ -37,8 +39,22 @@ const Analytics = () => {
         var today = new Date();
         var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
         var currentHour = today.getHours();
-        var minHourPerformance = today.getHours() - 12;
-        var maxHourPerformance = today.getHours() - 17;
+
+        var yesterdayDate = '';
+
+        if (today.getDate() === 1) {
+            if ((today.getMonth() + 1) === 1){ // El metodo getMonth() solo va de la posicion 0 a la 11, es por ello que deberemos sumarle '1' para que concuerde con el mes actual.
+                yesterdayDate = (today.getFullYear() - 1) + '-' + (today.getMonth() + 12) + '-' + (today.getDate() + 29);
+            }
+            else {
+                yesterdayDate = today.getFullYear() + '-' + today.getMonth() + '-' + (today.getDate() + 29);
+            }
+        }
+        else {
+            yesterdayDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate() - 1);
+        }
+
+        var lastHour = today.getHours() - 24;
 
 
         // --- HEADERS ELECTRICITY API REQUESTS --- //
@@ -64,10 +80,18 @@ const Analytics = () => {
 
         // --- PARAMS ELECTRICITY API REQUESTS --- //
 
-        const paramsAPIluz = {
+        const paramsAPIluzActual = {
             params: {
                 'start_date': currentDate + 'T00:00',
                 'end_date': currentDate + 'T23:59',
+                'time_trunc': 'hour'  
+            }
+        };
+
+        const paramsAPIluzAyer = {
+            params: {
+                'start_date': yesterdayDate + 'T00:00',
+                'end_date': yesterdayDate + 'T23:59',
                 'time_trunc': 'hour'  
             }
         };
@@ -112,16 +136,16 @@ const Analytics = () => {
             };
 
     
-            // --- BODY ENERGY SAVE API REQUESTS --- //
+            // --- BODY ENERGY SAVE & PERFORMANCE API REQUESTS --- //
 
-            const current_tank_temp_diff = {
-                "query": "SELECT inside FROM mqtt_consumer WHERE topic = 'ivanlab/solaris/S/2' AND time >= now() - " + currentHour + "h AND time <= now()",
+            const max_tank_temp = {
+                "query": "SELECT MAX(inside) FROM mqtt_consumer WHERE topic = 'ivanlab/solaris/S/2' AND time >= now() - " + currentHour + "h - 1d AND time <= now() + " + lastHour + "- 1d",
                 "type": "influxql",
                 "bucket": "sensors"
             };
 
-            const performance_temp_diff = {
-                "query": "SELECT inside FROM mqtt_consumer WHERE topic = 'ivanlab/solaris/S/2' AND time >= now() - " + minHourPerformance + "h - 1d AND time <= now() - " + maxHourPerformance + "h - 1d",
+            const min_tank_temp = {
+                "query": "SELECT MIN(inside) FROM mqtt_consumer WHERE topic = 'ivanlab/solaris/S/2' AND time >= now() - " + currentHour + "h - 1d AND time <= now() + " + lastHour + "- 1d",
                 "type": "influxql",
                 "bucket": "sensors"
             };
@@ -172,46 +196,70 @@ const Analytics = () => {
         );
 
 
-            // --- ENERGY SAVE API REQUESTS --- //
+            // --- ENERGY SAVE & PERFORMANCE API REQUESTS --- //
 
-            Axios.post('http://pablo-dev.ivanlab.lan:8086/api/v2/query?org=TFG_sensors', current_tank_temp_diff, headersDB).then(
+            Axios.post('http://pablo-dev.ivanlab.lan:8086/api/v2/query?org=TFG_sensors', max_tank_temp, headersDB).then(
                 (response) => {
                     console.log(response);
-                    var totalValues = response.data.results[0].series[0].values;
-                    setCurrentTankTempDiff(response.data.results[0].series[0].values[totalValues.length - 1][1] - response.data.results[0].series[0].values[0][1]);
+                    setmaxTankTemp(response.data.results[0].series[0].values[0][1]);
+                    setMaxTime(response.data.results[0].series[0].values[0][0]);
                 }
             );
 
-            Axios.post('http://pablo-dev.ivanlab.lan:8086/api/v2/query?org=TFG_sensors', performance_temp_diff, headersDB).then(
+            Axios.post('http://pablo-dev.ivanlab.lan:8086/api/v2/query?org=TFG_sensors', min_tank_temp, headersDB).then(
                 (response) => {
                     console.log(response);
-                    var totalValues = response.data.results[0].series[0].values;
-                    setPerformanceTempDiff(response.data.results[0].series[0].values[totalValues.length - 1][1] - response.data.results[0].series[0].values[0][1]);
+                    setMinTankTemp(response.data.results[0].series[0].values[0][1]);
+                    setMinTime(response.data.results[0].series[0].values[0][0]);
                 }
             );
 
 
         // --- ELECTRICITY API REQUESTS --- //
 
-        Axios.get('https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real', paramsAPIluz, headersAPIluz).then(
+        Axios.get('https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real', paramsAPIluzActual, headersAPIluz).then(
             (response) => {
                 console.log(response);
-                setLightPrice(response.data.included[0].attributes.values[currentHour].value);
-                setLightPriceGenTime(response.data.included[0].attributes.values[11].value);
+                setCurrentLightPrice(response.data.included[0].attributes.values[currentHour].value);
+            }
+        );
+
+        Axios.get('https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real', paramsAPIluzAyer, headersAPIluz).then(
+            (response) => {
+                console.log(response);
+                setLightPriceYesterdayGenTime(response.data.included[0].attributes.values[11].value);
             }
         );
 
         console.log(currentDate);
         console.log(currentHour);
+        console.log(yesterdayDate);
 
     };
 
 
     // --- POSTPROCESSED VARIABLES --- //
 
-        var energySaved = (300 * currentTankTempDiff) * 0.00116222;
-        var moneySaved = energySaved * (lightPriceGenTime/1000);
-        var realPerformance = (((performanceTempDiff/5) * 300) * 0.00116222);
+        var maxDate = new Date(maxTime);
+        var minDate = new Date(minTime);
+        
+
+        var tankDiff = 0;
+    
+        if (maxDate > minDate) {
+            tankDiff = maxTankTemp - minTankTemp;
+        }
+        else {
+            tankDiff = 0;
+        }
+
+
+        var energySaved = (300 * tankDiff) * 0.00116222;
+        var moneySaved = energySaved * (lightPriceYesterdayGenTime/1000);
+
+
+        var timeDiff = maxDate.getHours() - minDate.getHours();
+        var realPerformance = (((tankDiff/timeDiff) * 300) * 0.00116222);
         var expectedPerformance = (700 * 1.22)/1000;
         var performanceString = '';
 
@@ -225,8 +273,6 @@ const Analytics = () => {
             performanceString = 'Malo';
         }
         
-
-
     return (
         <div>
             Hola, obtén tus analíticas clicando en el siguiente botón. 
@@ -238,16 +284,20 @@ const Analytics = () => {
             Diferencial de activación: {diffTrigger}° <br/>
             Bombeo: {pump ? 'Activado' : 'No Activado'}<br/><br/>
 
-            Precio de la luz (Hora actual): {(lightPrice/1000).toFixed(3)} €/kWh <br/><br/>
+            Precio de la luz (Hora actual): {(currentLightPrice/1000).toFixed(3)} €/kWh <br/><br/>
 
-            Energía ahorrada (Para tanque de 300L): {energySaved.toFixed(3)} kW <br/><br/>
+            Energía ahorrada ayer (Para tanque de 300L): {energySaved.toFixed(3)} kW <br/><br/>
 
-            Dinero ahorrado hoy (Para el precio de generación a las 12 del mediodía): {moneySaved.toFixed(2)} € <br/><br/>
+            Dinero ahorrado durante el dia de ayer (Para el precio de generación a las 12 del mediodía): {moneySaved.toFixed(2)} € <br/><br/>
 
             Rendimiento de las placas solares: {performanceString} <br/>
-            {performanceTempDiff}° <br/>
+            {tankDiff}° <br/>
             {realPerformance} kW/h <br/>
-            {expectedPerformance} kW/h
+            {expectedPerformance} kW/h <br/><br/>
+
+            {timeDiff} <br/>
+            {maxDate.getHours()} <br/>
+            {minDate.getHours()}
 
         </div>
     );
